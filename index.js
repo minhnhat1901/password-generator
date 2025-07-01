c1896d75c3e7553ac26e06a5930ebdf02e383b71
Minh Duong <64678096+minhnhat1901@users.noreply.github.com>
2025-07-01 13:59:17 -0700

    Final version

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = [
+    "A","B","C","D","E","F","G","H","I","J","K","L",
+    "M","N","O","P","Q","R","S","T","U","V","W","X",
+    "Y","Z","a","b","c","d","e","f","g","h","i","j",
+    "k","l","m","n","o","p","q","r","s","t","u","v",
+    "w","x","y","z", "0", "1", "2", "3", "4", "5",
+    "6", "7", "8", "9","~","`","!","@","#","$","%","^",
+    "&","*","(",")","_","-","+","=","{","[","}","]",",",
+    "|",":",";","<",">",".","?","/"
+];
 
+const generatePassBtn = document.getElementById("pass-gen-btn");
+const pass1El = document.getElementById("pass-option1");
+const pass2El = document.getElementById("pass-option2");
+const passLengthRequestEl = document.getElementById("pass-length-request");
+const userInputEl = document.getElementById("user-input");
+// const passwordLength = 15;
+
+function generatePassword(length) {
+    let password1 = ""
+    let password2 = ""
+
+    for (let i = 0; i < length; i++) {
+        index1 = Math.floor(Math.random() * characters.length);
+        index2 = Math.floor(Math.random() * characters.length);
+
+        password1 += characters[index1];
+        password2 += characters[index2];
+    }
+
+    pass1El.textContent = password1;
+    pass2El.textContent = password2;
+}
+
+generatePassBtn.addEventListener("click", function() {
+    passLengthRequestEl.hidden = false;
+    generatePassBtn.textContent = "Generate Password";
+    generatePassword(userInputEl.value);
+});
